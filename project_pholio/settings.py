cc0e3eb94b8d59a56180b5c8d9865eecec1c134b
Ujjwaldeep <ujjwal2699singh@gmail.com>
2020-11-11 20:22:29 +0530

    About page added

--- project_pholio/settings.py
+++ project_pholio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (os.environ.get('DEGUB_TOGGLE') == 'True')
 
 
-ALLOWED_HOSTS = ['project-pholio.herokuapp.com']
+ALLOWED_HOSTS = ['project-pholio.herokuapp.com','pholio.tech']
 
 
 # Application definition
